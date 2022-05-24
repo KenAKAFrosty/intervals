f85736bd4d25a8ab233c6bc69d1c57259b6aaae9
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-05-23 22:33:07 -0400

    add: init

---
+++ index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+run();
+
+async function run(){ 
+    try{
+        const response = await axios.get("https://stream-raiders-tips.vercel.app/api/updateCurrentRaids");
+        console.log(response.data, new Date());
+    } catch(error) { 
+        console.log(error);
+        console.log('Received the above error on this attempt')
+    }
+    setTimeout( async ()=>{ await run() }, 17000)
+}
